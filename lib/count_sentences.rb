ef6304e9ae92dd748c6cd173977ea43100ecdc66
github username <github email address>
2017-07-20 09:27:41 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,26 @@ require 'pry'
 class String
 
   def sentence?
-    
+  	self[-1] == "."
   end
 
   def question?
-
+  	self[-1] == "?"
   end
 
   def exclamation?
-
+  	self[-1] == "!"
   end
 
   def count_sentences
+  	self.split(/\.|\?|\!/).delete_if {|w| w.size < 2}.size
 
+  	
+  	# (/\.|\?|\!/) - REGEX
+  	# (/        /) - empty set of regex
+  	#{ }" \.| "      - first item we are looking to split on is `.` 
+  	# ...
+  	
   end
+
 end
